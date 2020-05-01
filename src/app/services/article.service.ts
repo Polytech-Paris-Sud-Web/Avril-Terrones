e67fa2bb2cc1bdadf5f1b165b0679989fceeede1
thibaud_at <thibaud_at@yahoo.com>
2020-05-01 17:17:54 +0200

    change url for db

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -8,23 +8,26 @@ import {RawArticle} from "../models/raw-article";
 export class ArticleService {
 
   private _article : Observable<Article[]>;
+  
+  // private _url_db : string = "http://localhost:3000";
+  private _url_db : string = "https://my-json-server.typicode.com/Polytech-Paris-Sud-Web/Avril-Terrones";
 
   constructor(private http : HttpClient) {
   }
 
   public getAll(): Observable<Article[]> {
-    return this.http.get<Article[]>("http://localhost:3000/articles");
+    return this.http.get<Article[]>(`${this._url_db}/articles`);
   }
 
   public get(id:number): Observable<Article> {
-    return this.http.get<Article>(`http://localhost:3000/articles/${id}`);
+    return this.http.get<Article>(`${this._url_db}/articles/${id}`);
   }
 
   public delete(id:number): Observable<any> {
-    return this.http.delete<any>(`http://localhost:3000/articles/${id}`);
+    return this.http.delete<any>(`${this._url_db}/articles/${id}`);
   }
 
   public add(newArticle : RawArticle): Observable<Article> {
-    return this.http.post<Article>("http://localhost:3000/articles/", newArticle);
+    return this.http.post<Article>(`${this._url_db}/articles/`, newArticle);
   }
 }
